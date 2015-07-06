2d07b4e3e84759cd40eb60439a85872d733fb2ee
Victor Hugo Panisa <victor.panisa@gmail.com>
2015-07-05 21:30:37 -0300

    Cleaned up tons of lost files and finished fixing bugs on ezreal

--- game/dota_addons/dotl_dev/scripts/vscripts/heroes/hero_ezreal/ezreal_arcane_shift.lua
+++ game/dota_addons/dotl_dev/scripts/vscripts/heroes/hero_ezreal/ezreal_arcane_shift.lua
@@ -2,23 +2,22 @@ function ezreal_arcane_shift_on_spell_start(event)
 	local caster = event.caster
 	local ability = event.ability
 
-	ProjectileManager:ProjectileDodge(caster)
-	
+ 	ProjectileManager:ProjectileDodge(caster)
 	ParticleManager:CreateParticle("particles/items_fx/blink_dagger_start.vpcf", PATTACH_ABSORIGIN, caster)
 	caster:EmitSound("DOTA_Item.BlinkDagger.Activate")
-	
+
 	local origin_point = caster:GetAbsOrigin()
 	local target_point = event.target_points[1]
 	local difference_vector = target_point - origin_point
-	
+			
 	if difference_vector:Length2D() > event.blink_range then
 		target_point = origin_point + (target_point - origin_point):Normalized() * event.blink_range
 	end
-	
+
 	caster:SetAbsOrigin(target_point)
 	FindClearSpaceForUnit(caster, target_point, false)
-	
-	ParticleManager:CreateParticle("particles/items_fx/blink_dagger_end.vpcf", PATTACH_ABSORIGIN, caster)
+
+ 	ParticleManager:CreateParticle("particles/items_fx/blink_dagger_end.vpcf", PATTACH_ABSORIGIN, caster)
 
 	local nearby_enemy_units =
 		FindUnitsInRadius(
@@ -34,19 +33,22 @@ function ezreal_arcane_shift_on_spell_start(event)
 		)
 
 	local target = nearby_enemy_units[1]
-	local info = 
-	{
-		Target = target,
-		Source = caster,
-		Ability = ability,	
-		EffectName = event.effect_name,
-		vSourceLoc = caster:GetAbsOrigin(),
-		iMoveSpeed = event.projectile_speed,
-		bProvidesVision = false,
-		bDrawsOnMinimap = false,
-		bDodgeable = true,
-		bVisibleToEnemies = true,
-		bReplaceExisting = false
-	}
-	projectile = ProjectileManager:CreateTrackingProjectile(info)
+	if target ~= nil then
+		local info = 
+		{
+			Target = target,
+			Source = caster,
+			Ability = ability,	
+			EffectName = event.effect_name,
+			vSourceLoc = caster:GetAbsOrigin(),
+			iMoveSpeed = event.projectile_speed,
+			bProvidesVision = false,
+			bDrawsOnMinimap = false,
+			bDodgeable = true,
+			bVisibleToEnemies = true,
+			bReplaceExisting = false
+		}
+		projectile = ProjectileManager:CreateTrackingProjectile(info)
+	end
+	
 end

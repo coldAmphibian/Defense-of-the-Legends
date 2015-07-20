7daf542c09de184d1bc32e5ddf2783aa980623f9
Zarthbenn <kingmirazz@gmail.com>
2015-07-20 13:22:15 -0700

    Fixed Sona model + Added Some loadout portraits + fixed taric's w

--- game/dota_addons/dotl_dev/scripts/vscripts/heroes/hero_taric/taric_shatter.lua
+++ game/dota_addons/dotl_dev/scripts/vscripts/heroes/hero_taric/taric_shatter.lua
@@ -4,6 +4,7 @@ Author: Zarthbenn
 Date: 07/2015
 --]]
 
+
 function Shatter( event )
 	target = event.target
 	caster = event.caster
@@ -24,6 +25,34 @@ function Shatter( event )
 	ApplyDamage(damageTable)
 
 	ability:ApplyDataDrivenModifier(caster, target, "modifier_taric_shatter_debuff", nil)
+end
+
+--[[
+An overly complicated solution to something that could have been done with a simple function. (ApplyBonusArmor or something like that)
+This function creates a stack of modifier_taric_shatter_armor_percentage (one stack == 1 armor) for whatever 12 percent of taric's current armor is.
+--]]
+function ShatterApply( event )
+	caster = event.caster
+	ability = event.ability
+	target = event.target
+
+	armor = caster:GetPhysicalArmorValue() * 0.12
+	--Reduce one to compensate for the stack added at applydatadrivenmodifier()
+	armor = armor - 1
+
+	if armor <= 0 then
+		armor = 0
+	end
+
+	if target == nil then
+		ability:ApplyDataDrivenModifier(caster, target, "modifier_taric_shatter_armor_percentage", nil)
+		caster:SetModifierStackCount("modifier_taric_shatter_armor_percentage", caster, armor)
+	elseif target ~= nil then
+		ability:ApplyDataDrivenModifier(caster, target, "modifier_taric_shatter_armor_percentage", nil)
+		target:SetModifierStackCount("modifier_taric_shatter_armor_percentage", caster, armor)
+	else
+		print("Error")
+	end
 end
 
 function ShatterCheck( event )

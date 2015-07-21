9b4c87b1f963fac6c2351d934cc529a741502839
Cold Amphibian <coldamphibian@gmail.com>
2015-07-22 02:05:42 +1000

    Added my paintings to custom loading screen. Fixed code to work with new file names. Removed placeholder Diana and Leonas. Left originals in just in case.

--- content/dota_addons/dotl_dev/panorama/scripts/custom_game/custom_loading_screen.js
+++ content/dota_addons/dotl_dev/panorama/scripts/custom_game/custom_loading_screen.js
@@ -1,6 +1,5 @@
-var images = [["Leona.png","Diana.png"],["Leona1.png","Diana1.png"],["Leona2.png","Diana2.png"]];
+var images = [["Rubick.png","Viktor.png"],["Techies.png","Teemo.png"],["Phoenix.png","Anivia.png"],["Enigma.png","Bard.png"]];
 var pair = Math.floor(Math.random()*images.length);
 
 $("#PortraitLeft").SetImage("file://{images}/custom_game/loading_screen/" + images[pair][0]);
 $("#PortraitRight").SetImage("file://{images}/custom_game/loading_screen/" + images[pair][1]);
-$.Msg(pair);

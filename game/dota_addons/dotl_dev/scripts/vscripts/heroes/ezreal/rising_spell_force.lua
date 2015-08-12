c8614655436472acc9cd280d4eec9f35c474e8ea
Victor Hugo Panisa <victor.panisa@gmail.com>
2015-08-11 22:51:03 -0300

    Removing hard coded things

--- game/dota_addons/dotl_dev/scripts/vscripts/heroes/ezreal/rising_spell_force.lua
+++ game/dota_addons/dotl_dev/scripts/vscripts/heroes/ezreal/rising_spell_force.lua
@@ -6,10 +6,11 @@
 function OnProjectileHitUnit(event)
 	local caster = event.caster
 	local innate = caster:FindAbilityByName("ezreal_rising_spell_force")
+	local duration = innate:GetLevelSpecialValueFor('buff_duration', 0)
 	if innate ~= nil then
 		local current_stack = caster:GetModifierStackCount("modifier_rising_spell_force", nil)	
 		caster:RemoveModifierByName("modifier_rising_spell_force")
-		innate:ApplyDataDrivenModifier(caster, caster, "modifier_rising_spell_force", {duration = 6})
+		innate:ApplyDataDrivenModifier(caster, caster, "modifier_rising_spell_force", {duration = duration})
 		local max_stack = innate:GetLevelSpecialValueFor("max_stacks", 0)
 		if current_stack >= max_stack then
 			caster:SetModifierStackCount("modifier_rising_spell_force", innate, max_stack)
